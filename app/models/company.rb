395f3de1f0c68038bbacd18a1ef5d76dd3ba5b81
Corey <102438938+C-Zhee@users.noreply.github.com>
2023-01-19 11:26:29 -0500

    Update company.rb

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,15 @@
 class Company < ActiveRecord::Base
+    has_many :freebies 
+    has_many :dev through :freebies
 
+    def give_freebie (dev, item_name, value)
+        Freebie.create (item_name: item_name,  
+                        value: value, 
+                        dev: dev, 
+                        company: self)
+    end
+end
+
+def self.oldest_company
+    Company.all.order(:founding_year).first
 end
