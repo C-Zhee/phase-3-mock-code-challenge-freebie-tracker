48707463bdd2d1067706e87b6b5af3fc23d83edf
Corey <102438938+C-Zhee@users.noreply.github.com>
2023-01-19 11:26:49 -0500

    Update dev.rb

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies through: :freebies
 
+    def recieved_one? (item_name)
+        freebies.map do |a|
+            a.item_name
+        end
+
+    def give_away(freebie, dev)
+       
 end
+
+
